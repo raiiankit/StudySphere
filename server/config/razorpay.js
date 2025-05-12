450481baa3f9daae04bb47e58159c8df60039a92
Ankit Rai <ankit.rai@bypeopletechnologies.com>
2025-05-12 23:54:39 +0530

    intial !!

---
+++ server/config/razorpay.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const Razorpay = require("razorpay");
+
+console.log("Razorpay Key:", process.env.RAZORPAY_KEY_ID)
+console.log("Razorpay Secret:", process.env.RAZORPAY_SECRET)
+
+
+const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,     
+    key_secret: process.env.RAZORPAY_SECRET, 
+});
+
+module.exports = instance;  
---
+++ src/data/navbar-links.js
@@ -0,0 +1,18 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Catalog",
+    path: '/category-page',
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+];
